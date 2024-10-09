054725395f073eb534b091ab6f3c99134faeced3
Alberto <coolPenwin@proton.me>
2024-10-09 18:14:10 +0000

    home

--- src/front/js/pages/homePage.js
+++ src/front/js/pages/homePage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useContext } from "react";
 import { Context } from "../store/appContext"; // Ajusta la ruta según sea necesario
 import { useNavigate } from "react-router-dom";
+import { CardArticle } from "../component/cardArticle";
+
 
 export const HomePage = () => {
     const { store, actions } = useContext(Context); // Obtiene el store y las acciones
@@ -15,12 +17,30 @@ export const HomePage = () => {
         } else {
             // Si el token está presente, solicita el contenido de la homepage
             actions.getHomepage();
+            actions.getDataArticle();
         }
     }, []);
 
     return (
         <div className="container mt-5">
-            <h2 className="text-center">Bienvenido a la Homepage</h2>
+            			<h1 className="text-danger">HOMEE privadoo</h1>
+			<div className="row d-flex flex-nowrap my-5" style={{ overflowX: "scroll" }}>
+				{store.Articles.map((article, index) => (
+					<CardArticle
+						key={index}
+						title={article.title}
+						content={article.content}
+						image={article.image}
+						published_date={article.published_date}
+						source={article.source}
+						link={article.link}
+						author={article.author.name}
+						newspaper={article.newspaper.name}
+						category={article.category.name}
+						id={article.id}
+					/>
+				))}
+			</div>
             {store.homepageMessage ? (
                 <p className="mt-4">{store.homepageMessage}</p>
             ) : (
